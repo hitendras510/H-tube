8e77a5aca63a056c75cb120a6e33ef050a36b9a2
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-03 11:34:52 +0530

    feat: initialize h-tube project with basic structure and dependencies
    - Added package.json with necessary dependencies and scripts.
    - Created .env.sample for environment variables.
    - Implemented main application file (app.js) with middleware and route setup.
    - Established database connection using mongoose.
    - Developed user and healthcheck controllers with basic functionality.
    - Created models for user, video, comment, like, playlist, subscription, and tweet.
    - Set up routes for user registration and healthcheck.
    - Added utility functions for error handling and API responses.
    - Integrated Cloudinary for image uploads.

---
+++ h-tube/src/models/video.models.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import 
+
+mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema({
+  videoFile: {
+    type: String,
+    required: true,
+  },
+  thubnail: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    default: 0,
+  },
+  isPublished: {
+      type: String,
+      deafult: true,
+    },
+    owner: {
+        type: Schema.types.Objectid,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
