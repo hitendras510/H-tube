3ca90bd723f6a47afa496ebf9a9fe1268c92ec1c
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-03 12:20:48 +0530

    feat: enhance user registration with avatar upload and error handling

--- h-tube/src/controllers/user.controllers.js
+++ h-tube/src/controllers/user.controllers.js
@@ -1,5 +1,8 @@
  import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
  const registerUser = asyncHandler( async (req,res)=> {
     const {fullName,username,email,password} = req.body
@@ -15,10 +18,46 @@ import { ApiError } from "../utils/ApiError.js";
         $or: [{username}, {email}]
      })
  if (existedUser) {
-   throw new ApiError(401, "User already exists");
+   throw new ApiError(401, "User already exists")
  }
  
- })
+ const avatarLocalPath = req.files?.avatar[0]?.path
+ const coverLocalPath = req.files?.coverImage[0]?.path
+
+ if (!avatarLocalPath) {
+      throw new ApiError(402, "avatar file is missing")
+ }
+
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+let coverImage= ""
+if (coverImage) {
+    
+coverImage = await uploadOnCloudinary(coverImage)
+}
+
+const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+})
+
+await User.finById(user._id).select(
+    "-password -refreshToken"
+)
+
+if(!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering a user")
+}
+
+return res
+.status(201)
+.json( new ApiResponse(200, createdUser,"User registration successfull"))
+
+
+})
 
 
 
